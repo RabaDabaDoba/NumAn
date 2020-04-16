9896c5b52b8b2e4ac4c2361e6d92dde0b46cd709
RabaDabaDoba <robindah@kth.se>
2020-04-16 17:06:52 +0200

    up

--- Lab2/U2.m
+++ Lab2/U2.m
@@ -12,51 +12,93 @@ plot(x, f(x), 'r');
 title("Area: " + q);
 
 %b
+% format short
+% n = 10;
+% limits = [-1 1]; %a b
+% %h = (limits(2)-limits(1))/n %Ska vara myckte mindre än 1;
+% hs = [1, 0.5, 0.25, 0.125, 0.0625]
+% h = hs(5);
+% Ih = 0;
+% counter = 0;
+% disp("Counter     increment    x value     I    |T(h) - I|");
+% sum = abs(limits(1)) + abs(limits(2));
+% T = zeros(1,1/h);
+% error = zeros(1,1/h);
+% while (h*counter <= 1)
+%     
+%     position = 0;
+%     increment = 0;
+%     if(counter == 0 || counter*h == 1) %Special fallen
+%         if(counter == 0)
+%             position = limits(1);
+%             increment = f(limits(1))/2;
+%             Ih = Ih + increment;
+%             
+%         end
+%         if(counter*h == 1)
+%             position = limits(2);
+%             increment = f(limits(2))/2;
+%             Ih = Ih + increment;
+%         end
+%         
+%     else
+%         T(counter-1) = h*((f(limits(1) + counter*h*sum) + limits(1) + (counter+1)*h*sum)/2);
+%         position = limits(1) + counter*h*sum;
+%         increment = f(limits(1) + counter*h*sum);
+%         Ih = Ih + increment;
+%         
+%     end 
+%     %drawRect(limits(1) + counter*h*sum,0,limits(1) + (counter+1)*h*sum,f(limits(1) + (counter+1)*h*sum));
+%     error(counter+1) = abs(T(counter+1) - q);
+%     disp([counter increment position Ih error(counter+1)]);
+%     counter = counter + 1;
+% end
+% disp("     Res   2x Res")
+% %Varför är den för liten???
+% resultat = Ih*h;
+% disp([resultat, resultat*2])
+
 format short
-n = 10;
+n = 100;
 limits = [-1 1]; %a b
-h = (limits(2)-limits(1))/n %Ska vara myckte mindre än 1;
-%h = 0.5;
-I = 0;
-counter = 0;
-disp("Counter     increment    x value     I    |T(h) - I|");
+%h = (limits(2)-limits(1))/n %Ska vara myckte mindre än 1;
+hs = [1, 0.5, 0.25, 0.125, 0.0625]
+h = hs(5);
+Ih = 0;
+counter = 1;
+disp("Counter     increment    x value     Ih    |T(h) - I|");
 sum = abs(limits(1)) + abs(limits(2));
 T = zeros(1,1/h);
-while (h*counter <= 1)
-    T(counter + 1) = h*((f(limits(1) + counter*h*sum) + limits(1) + (counter + 1)*h*sum)/2);
+error = zeros(1,1/h);
+
+position0 = limits(1);
+increment0 = f(limits(1))/2;
+Ih = Ih + increment0;
+disp([0 increment0 position0 increment0 abs(increment0-q)]);
+
+positionN = limits(2);
+incrementN = f(limits(2))/2;
+%Ih = Ih + increment;
+
+while (h*counter < 1)  %Lägger till xN på slutet
     position = 0;
     increment = 0;
-    if(counter == 0 || counter*h == 1) %Special fallen
-        if(counter == 0)
-            position = limits(1);
-            increment = f(limits(1))/2;
-            I = I + increment;
-            
-        end
-        if(counter*h == 1)
-            position = limits(2);
-            increment = f(limits(2))/2;
-            I = I + increment;
-        end
-        
-    else
-        position = limits(1) + counter*h*sum;
-        increment = f(limits(1) + counter*h*sum);
-        I = I + increment;
-        
-    end 
-    %drawRect(limits(1) + counter*h*sum,0,limits(1) + (counter+1)*h*sum,f(limits(1) + (counter+1)*h*sum));
-    disp([counter increment position I abs(T(counter+1) - increment)]);
-    counter = counter + 1;
+    %T(counter-1) = h*((f(limits(1) + counter*h*sum) + limits(1) + (counter+1)*h*sum)/2);
+    position = limits(1) + counter*h*sum;
+    increment = f(limits(1) + counter*h*sum);
+    Ih = Ih + increment;
+    
+
+
+error(counter) = abs(increment - q);
+disp([counter increment position Ih error(counter)]);
+counter = counter + 1;
 end
+disp([((1/h)) incrementN positionN Ih abs(incrementN-q)]);
+disp("     Res   2x Res")
+resultat = (Ih + incrementN)*h;
+disp([resultat, resultat*2])
 
-Ih = I *h
-2*Ih
-
-
-function drawRect(a, b, c, d)
-rectangle('Position',[a b c d])
-end
 
 %Fattar inte vad jag gör för fel med om man tar gånger 2 så får man det
 %rätta värdet.
--- Lab2/U2_Simpsons.m
+++ Lab2/U2_Simpsons.m
@@ -1,37 +1,41 @@
+syms x;
 x = -2:0.1:2;
 a = -1:0.1:1;
 f = @(x) sqrt(x+2);
+q = integral(f, -1, 1)
 
 n = 1000000;
 limits = [-1 1]; %a b
-h = (limits(2)-limits(1))/n %Ska vara myckte mindre än 1;
+%h = (limits(2)-limits(1))/n %Ska vara myckte mindre än 1;
 %h = 0.5;
+hs = [1, 0.5, 0.25, 0.125, 0.0625, 0.0001];
+h = hs(6);
 I = 0;
 counter = 0;
 %disp(["Counter" "increment" "x value" "I" "|T(h) - I|"]);
 sum = abs(limits(1)) + abs(limits(2));
 
-
-
 summaOdd = 0;
 summaEven = 0;
-
-
+disp("Error");
 
 while (h*counter <= 1)
     %h*counter
     currentValue = limits(1) + counter*h*sum;
-    
+    increment = 0;
     if(rem(counter, 2) == 0)
         %Jämna
         %disp("Even");
-        summaEven = summaEven + f(currentValue);
+        incr(counter+1) = f(currentValue);
+        summaEven = summaEven + incr(counter+1);
     else
         %Udda
         %disp("Odd");
-        summaOdd = summaOdd + f(currentValue);
+        incr(counter+1) = f(currentValue);
+        summaOdd = summaOdd + incr(counter+1);
     end
-
+    error(counter+1) = abs(incr(counter+1) - q);
+    disp(error(counter+1) );
     counter = counter + 1;
 end
 
